56f16bfbfdad213d1f5ef7f8c8f8af6b583b6d50
K3n <kalyankaushik.khasibhatla@gmail.com>
2023-11-20 22:32:55 +0530

    New python project - update ow

--- main.py
+++ main.py
@@ -14,6 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('Hello')
     print_hi('namaste')
-    print_hi('New Code')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
